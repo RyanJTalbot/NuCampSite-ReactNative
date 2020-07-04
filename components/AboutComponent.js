9b340dcbedd0e047ac8ccf0ae5700b093478566d
Ryan JT <ryantalbot7@gmail.com>
2020-07-04 12:31:56 -0500

    Initialize repository

---
+++ components/AboutComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text, ScrollView } from "react-native";
+
+class AboutComponent extends React.Component {
+  render() {
+    return (
+      
+    )
+  }
+}
+export default AboutComponent;
