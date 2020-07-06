365527c8053e5159eb9d5930565dac64a07ef704
Ryan JT <ryantalbot7@gmail.com>
2020-07-05 20:44:38 -0500

    Fixed bugs

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -41,7 +41,7 @@ class Home extends Component {
         return (
             <ScrollView>
                 <RenderItem item={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]} />
-                <RenderItem item={this.props.promotions.Cardpromotinos.filter(promotion => promotion.featured)[0]} />
+                <RenderItem item={this.props.promotions.promotions.filter(promotion => promotion.featured)[0]} />
                 <RenderItem item={this.props.partners.partners.filter(partner => partner.featured)[0]} />
             </ScrollView>
         )
